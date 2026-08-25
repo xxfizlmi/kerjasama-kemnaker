813380e7864e8179880fd58c66cb93eb22814811
Hafiz Ilmi <asj.hi55@gmail.com>
2026-08-25 10:43:16 +0700

    update body

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -14,28 +14,29 @@ $this->render('_head');
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>" class="h-100" data-bs-theme="light">
+
 <head>
     <?php $this->head() ?>
     <title><?= Html::encode($this->title) ?></title>
 </head>
-<body class="d-flex flex-column h-100">
-<?php $this->beginBody() ?>
 
-<?= $this->render('_header') ?>
+<body class="d-flex flex-column min-vh-100">
+    <?php $this->beginBody() ?>
 
-<main id="main" class="flex-grow-1" role="main">
-    <div class="container">
+    <?= $this->render('_header') ?>
+
+    <main id="main" class="flex-grow-1" role="main">
         <?php if (!empty($this->params['breadcrumbs'])): ?>
             <?= Breadcrumbs::widget(['links' => $this->params['breadcrumbs']]) ?>
         <?php endif ?>
         <?= Alert::widget() ?>
         <?= $content ?>
-    </div>
-</main>
+    </main>
 
-<?= $this->render('_footer') ?>
+    <?= $this->render('_footer') ?>
 
-<?php $this->endBody() ?>
+    <?php $this->endBody() ?>
 </body>
+
 </html>
 <?php $this->endPage() ?>
