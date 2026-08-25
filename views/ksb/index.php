32defb07755162d4ce7f2342d956283ef8ec1c41
Hafiz Ilmi <asj.hi55@gmail.com>
2026-08-25 10:40:48 +0700

    update ksb : revisi dan modal baru

--- views/ksb/index.php
+++ views/ksb/index.php
@@ -23,58 +23,181 @@ $section = [
 
 $cards = [
     [
+        'id' => 'japan',
         'image' => '@web/images/countries/japan.png',
-        'title' => 'JEPANG',
+        'name' => 'JEPANG',
         'documentCount' => 6,
-        'url' => ['/ksdn/detail', 'id' => 3],
+        'items' => [
+            [
+                'title' => 'PENINGKATAN PENGIRIMAN DAN PENERIMAAN PESERTA MAGANG TEKNIS INDONESIA',
+                'description' => 'memanfaatkan sumber daya dan keunggulan Para Pihak guna mempromosikan secara efektif pengembangan, pengiriman, dan pen...',
+                'status' => 'Aktif',
+                'mitra' => 'Prefektur Kagawa',
+                'pemerintah' => 'Pemerintah'
+            ],
+            [
+                'title' => 'PENINGKATAN PENGIRIMAN DAN PENERIMAAN PESERTA MAGANG TEKNIS INDONESIA',
+                'description' => '1. penyediaan dukungan dalam proses pengembangan, pengiriman, dan penerimaan Peserta Magang Teknis Indonesia di Prefekt...',
+                'status' => 'Aktif',
+                'mitra' => 'Pemerintah Prefektur Ehime ',
+                'pemerintah' => 'Pemerintah'
+            ],
+            [
+                'title' => 'Memorandum Saling Pengertian antara Kementerian Ketenagakerjaan Republik Indonesia dan Pemerintah Prefektur Mie Jepang tentang Kerja Sama untuk Meningkatkan Pengiriman dan Penerimaan Peserta Pemagangan Teknis Indonesia ke Jepang',
+                'description' => 'Para Pihak sepakat untuk bekerja sama dalam kegiatan berikut: a. pemberian dukungan dalam proses pengembangan, pengirim...',
+                'status' => 'Aktif',
+                'mitra' => 'Prefektur Mie',
+                'pemerintah' => 'Pemerintah'
+            ],
+            [
+                'title' => 'Memorandum Kerja Sama antara Kementerian Ketenagakerjaan Republik Indonesia dan Badan Kerja Sama Internasional Jepang tentang Pengembangan Sumber Daya Manusia Tenaga Kerja Indonesia',
+                'description' => '',
+                'status' => 'Aktif',
+                'mitra' => 'JICA (Japan International Cooperation Agency) ',
+                'pemerintah' => 'Pemerintah Negara Asing'
+            ],
+            [
+                'title' => 'Memorandum Kerja Sama antara Sekretariat Jenderal Kementerian Ketenagakerjaan Republik Indonesia dan Pemerintah Prefektur Miyagi Jepang tentang Percepatan Penempatan dan Penerimaan Pemagangan Teknis Indonesia, Sfecified Skilled Workers dan Teknisi ke Jepang',
+                'description' => 'Tujuan dari Memorandum Kerja Sama (MOC) ini adalah untuk mempercepat fasilitasi yang lebih efektif bagi program pelatiha...',
+                'status' => 'Aktif',
+                'mitra' => 'Prefektur Miyagi',
+                'pemerintah' => 'Pemerintah'
+            ],
+            [
+                'title' => 'Memorandum Kerja Sama tentang Program Pelatihan Praktek Kerja Teknis antara Pemerintah Republik Indonesia dengan Pemerintah Jepang',
+                'description' => 'Tujuan kerjasama ini adalah untuk mentransfer keterampilan teknis, dll. dari Jepang ke Republik Indonesia secara tepat d...',
+                'status' => 'Aktif',
+                'mitra' => 'Pemerintah Jepang ',
+                'pemerintah' => 'Pemerintah'
+            ],
+        ],
     ],
     [
+        'id' => 'albania',
         'image' => '@web/images/countries/albania.png',
-        'title' => 'ALBANIA',
+        'name' => 'ALBANIA',
         'documentCount' => 1,
-        'url' => ['/ksdn/detail', 'id' => 3],
+        'items' => [
+            [
+                'title' => 'Pernyataan Kehendak antara Kementerian Ketenagakerjaan Republik Indonesia dan Kementerian Ekonomi, Kebudayaan dan Inovasi Republik Albania tentang Kerja Sama Tenaga Kerja dan Ketenagakerjaan',
+                'description' => 'Para pihak akan mengeksplorasi kolaborasi untuk mengembangkan pengaturan khusus di bidang kerja sama berikut: a. mening...',
+                'status' => 'Aktif',
+                'mitra' => 'Kementerian Ekonomi, Kebudayaan dan Inovasi Republik Albania',
+                'pemerintah' => 'Pemerintah'
+            ],
+        ],
     ],
     [
+        'id' => 'palestine',
         'image' => '@web/images/countries/palestine.png',
-        'title' => 'PALESTINA',
+        'name' => 'PALESTINA',
         'documentCount' => 1,
-        'url' => ['/ksdn/detail', 'id' => 3],
+        'items' => [
+            [
+                'title' => 'Pernyataan Kehendak antara Kementerian Ketenagakerjaan Republik Indonesia dan Kementerian Tenaga Kerja Negara Palestina tentang Kerja Sama di bidang Tenaga Kerja dan Pengembangan Kapasitas',
+                'description' => 'HEREBY DECLARE the Participants intention to establish a cooperation in the field of labor with the following scopes of...',
+                'status' => 'Aktif',
+                'mitra' => 'Kementerian Tenaga Kerja Negara Palestina',
+                'pemerintah' => 'Pemerintah Negara Asing'
+            ],
+        ],
     ],
     [
+        'id' => 'turkey',
         'image' => '@web/images/countries/turkey.png',
-        'title' => 'TURKI',
+        'name' => 'TURKI',
         'documentCount' => 1,
-        'url' => ['/ksdn/detail', 'id' => 3],
+        'items' => [
+            [
+                'title' => 'Nota Kesepahaman tentang Kerja Sama di Bidang Ketenagakerjaan antara Kementerian Ketenagakerjaan Republik Indonesia dan Kementerian Tenaga Kerja dan Jaminan Sosial Republik Turki',
+                'description' => 'Para Pihak memutuskan untuk bekerja sama dalam bidang-bidang berikut: a) hubungan kerja yang mencakup hubungan pekerja...',
+                'status' => 'Aktif',
+                'mitra' => 'Kementerian Tenaga Kerja dan Jaminan Sosial Republik Turki',
+                'pemerintah' => 'Pemerintah Negara Asing'
+            ],
+        ],
     ],
     [
+        'id' => 'swiss',
         'image' => '@web/images/countries/swiss.png',
-        'title' => 'SWISS',
+        'name' => 'SWISS',
         'documentCount' => 2,
-        'url' => ['/ksdn/detail', 'id' => 3],
+        'items' => [
+            [
+                'title' => 'Persetujuan antara Pemerintah Republik Indonesia dan Dewan Federal Swiss pada Pertukaran Profesional Muda',
+                'description' => '1. Persetujuan ini mengatur pertukaran warga negara Swiss dan Indonesial (dari kedua jenis kelamin) yang bekerja di nega...',
+                'status' => 'Aktif',
+                'mitra' => 'Pemerintah Jepang ',
+                'pemerintah' => 'Pemerintah Negara Asing'
+            ],
+            [
+                'title' => 'Memorandum Saling Pengertian antara Kementerian Ketenagakerjaan Republik Indonesia dan Departemen Federal Urusan Ekonomi, Pendidikan dan Penelitian Konfederasi Swiss tentang Kerja Sama di Bidang Tenaga Kerja dan Hubungan Kerja',
+                'description' => '1. Para Penandatangan akan bekerjasama di bidang sebagai berikut : a) Kebijakan pasar tenaga kerja; b) Hukum Ketenag...',
+                'status' => 'Aktif',
+                'mitra' => 'State Secretariat for Economic Affairs (SECO)',
+                'pemerintah' => 'Pemerintah Negara Asing'
+            ],
+        ],
     ],
     [
+        'id' => 'singapore',
         'image' => '@web/images/countries/singapore.png',
-        'title' => 'SINGAPURA   ',
-        'documentCount' => 2,
-        'url' => ['/ksdn/detail', 'id' => 3],
+        'name' => 'SINGAPURA   ',
+        'documentCount' => 1,
+        'items' => [
+            [
+                'title' => 'Nota Kesepahaman antara Kementerian Koordinator Bidang Perekonomian Republik Indonesia dan Kementerian Perdagangan dan Perindustrian Republik Singapura tentang Program Tech:X Indonesia - Singapura',
+                'description' => 'Tujuan Nota Kesepahaman ("MOU") ini adalah untuk menetapkan kerangka kerja untuk Program Tech:X antara Indones...',
+                'status' => 'Aktif',
+                'mitra' => 'Kementerian Perdagangan dan Perindustrian Republik Singapura ',
+                'pemerintah' => 'Pemerintah Negara Asing'
+            ],
+        ],
     ],
     [
+        'id' => 'ajerbaijan',
         'image' => '@web/images/countries/azerbaijan.png',
-        'title' => 'AJERBAIJAN',
+        'name' => 'AJERBAIJAN',
         'documentCount' => 2,
-        'url' => ['/ksdn/detail', 'id' => 3],
+        'items' => [
+            [
+                'title' => 'Memorandum Saling Pengertian antara Kementerian Tenaga Kerja dan Perlindungan Sosial Penduduk Republik Azerbaijan dan Kementerian Ketenagakerjaan Republik Indonesia tentang Kerja Sama di Bidang Tenaga Kerja, Ketenagakerjaan, dan Perlindungan Sosial',
+                'description' => 'Para Pihak wajib bekerja sama dalam bidang-bidang utama berikut dalam kompetensinya sesuai dengan undang-undang nasional...',
+                'status' => 'Aktif',
+                'mitra' => 'Kementerian Tenaga Kerja dan Perlindungan Sosial Penduduk Republik Azerbaijan',
+                'pemerintah' => 'Pemerintah'
+            ],
+        ],
     ],
     [
+        'id' => 'austria',
         'image' => '@web/images/countries/austria.png',
-        'title' => 'AUSTRIA',
+        'name' => 'AUSTRIA',
         'documentCount' => 2,
-        'url' => ['/ksdn/detail', 'id' => 3],
+        'items' => [
+            [
+                'title' => 'Memorandum Saling Pengertian antara Kementerian Ketenagakerjaan Republik Indonesia dan Kementerian Federal Tenaga Kerja dan Ekonomi Republik Austria tentang Kerja Sama Pelatihan Vokasi pada Pelatihan Berbasis Kerja',
+                'description' => '1. Memorandum Saling Pengertian ini (selanjutnya disebut MSP) merupakan kerangka kerja sama antara Para Pihak di bidang...',
+                'status' => 'Aktif',
+                'mitra' => 'Kementerian Federal Tenaga Kerja dan Ekonomi Republik Austria ',
+                'pemerintah' => 'Pemerintah Negara Asing'
+            ],
+        ],
     ],
     [
+        'id' => 'australia',
         'image' => '@web/images/countries/australia.png',
-        'title' => 'AUSTRALIA',
+        'name' => 'AUSTRALIA',
         'documentCount' => 2,
-        'url' => ['/ksdn/detail', 'id' => 3],
+        'items' => [
+            [
+                'title' => 'Memorandum Saling Pengertian (Sebagaimana Telah Diubah) antara Pemerintah Republik Indonesia dan Pemerintah Australia tentang Pilot Pertukaran Pengembangan Keterampilan',
+                'description' => 'Tujuan dari Memorandum Saling Pengertian (sebagaimana telah diubah) (MSP) ini adalah untuk: (a) menyelenggarakan Pilot...',
+                'status' => 'Aktif',
+                'mitra' => 'Pemerintah Australia',
+                'pemerintah' => 'Pemerintah'
+            ],
+        ],
     ],
 ];
 
@@ -115,5 +238,8 @@ $cards = [
         <?= Html::endTag('div') ?>
 
     <?= Html::endTag('div') ?>
+        <?php foreach ($cards as $card): ?>
+        <?= $this->render('../components/ui/modal', array_merge($card, ['type' => 'ksb'])) ?>
+    <?php endforeach; ?>
 
 <?= Html::endTag('div') ?>
