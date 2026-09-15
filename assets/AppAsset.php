639bcc0c49df95e0957c8dd7e1fc7b1a238d0ccf
Hafiz Ilmi <asj.hi55@gmail.com>
2026-09-15 09:11:32 +0700

    fix css paths on vercel

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -10,7 +10,7 @@ use yii\web\View;
 class AppAsset extends AssetBundle
 {
     public $basePath = '@app/web';
-    public $baseUrl = '';
+    public $baseUrl = '/';
 
     public $css = [
         'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
