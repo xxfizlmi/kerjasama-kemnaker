df220291e40a5a84f93480263c3158f223fe876b
Hafiz Ilmi <asj.hi55@gmail.com>
2026-09-15 08:34:08 +0700

    "add detailed yii vercel logging"

--- api/index.php
+++ api/index.php
@@ -2,27 +2,45 @@
 
 declare(strict_types=1);
 
-defined('YII_DEBUG') or define('YII_DEBUG', false);
-defined('YII_ENV') or define('YII_ENV', 'prod');
+error_reporting(E_ALL);
+ini_set('display_errors', '0');
+ini_set('log_errors', '1');
 
-$root = dirname(__DIR__);
+register_shutdown_function(function () {
+    $error = error_get_last();
 
-require $root . '/vendor/autoload.php';
-require $root . '/vendor/yiisoft/yii2/Yii.php';
-
-$config = require $root . '/config/web.php';
-
-$config['runtimePath'] = '/tmp/yii-runtime';
-$config['components']['request']['baseUrl'] = '';
+    if ($error !== null) {
+        error_log(
+            'PHP SHUTDOWN ERROR: ' .
+                $error['message'] .
+                ' in ' .
+                $error['file'] .
+                ':' .
+                $error['line']
+        );
+    }
+});
 
 try {
+    defined('YII_DEBUG') or define('YII_DEBUG', false);
+    defined('YII_ENV') or define('YII_ENV', 'prod');
+
+    $root = dirname(__DIR__);
+
+    require $root . '/vendor/autoload.php';
+    require $root . '/vendor/yiisoft/yii2/Yii.php';
+
+    $config = require $root . '/config/web.php';
+
+    $config['runtimePath'] = '/tmp/yii-runtime';
+    $config['components']['request']['baseUrl'] = '';
+
     (new yii\web\Application($config))->run();
 } catch (\Throwable $e) {
-    error_log(
-        get_class($e) . ': ' .
-            $e->getMessage() . PHP_EOL .
-            $e->getTraceAsString()
-    );
+    error_log('YII ERROR: ' . get_class($e));
+    error_log('MESSAGE: ' . $e->getMessage());
+    error_log('FILE: ' . $e->getFile() . ':' . $e->getLine());
+    error_log('TRACE: ' . $e->getTraceAsString());
 
     http_response_code(500);
     echo 'Internal Server Error';
