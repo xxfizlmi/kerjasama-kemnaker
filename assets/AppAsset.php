130b3646468e97bf325d34ff02b9eead48a19655
Hafiz Ilmi <asj.hi55@gmail.com>
2026-09-16 14:29:48 +0700

    fix yii assets for vercel

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -1,44 +1,33 @@
 <?php
 
-/**
- * @link https://www.yiiframework.com/
- * @copyright Copyright (c) 2008 Yii Software LLC
- * @license https://www.yiiframework.com/license/
- */
-
 declare(strict_types=1);
 
 namespace app\assets;
 
-use yii\bootstrap5\BootstrapAsset;
 use yii\web\AssetBundle;
 use yii\web\View;
-use yii\web\YiiAsset;
 
-/**
- * Main application asset bundle.
- *
- * @author Qiang Xue <qiang.xue@gmail.com>
- * @since 2.0
- */
 class AppAsset extends AssetBundle
 {
-    public $basePath = '@webroot';
-    public $baseUrl = '@web';
+    public $basePath = '@app/web';
+    public $baseUrl = '/';
+
     public $css = [
-        'css/style.css',
+        'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
         'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css',
         'https://unpkg.com/aos@next/dist/aos.css',
+        'css/style.css',
     ];
+
     public $js = [
-        'js/script.js',
+        'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js',
         'https://unpkg.com/aos@next/dist/aos.js',
+        'js/script.js',
     ];
+
     public $jsOptions = [
         'position' => View::POS_HEAD,
     ];
-    public $depends = [
-        YiiAsset::class,
-        BootstrapAsset::class,
-    ];
+
+    public $depends = [];
 }
