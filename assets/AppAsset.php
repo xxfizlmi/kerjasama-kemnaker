04a50df93afbaf850687a40783962ce07dc970f6
Hafiz Ilmi <asj.hi55@gmail.com>
2026-09-16 14:09:26 +0700

    update asset "index"

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -27,13 +27,11 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/style.css',
-        'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
         'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css',
         'https://unpkg.com/aos@next/dist/aos.css',
     ];
     public $js = [
         'js/script.js',
-        'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js',
         'https://unpkg.com/aos@next/dist/aos.js',
     ];
     public $jsOptions = [
